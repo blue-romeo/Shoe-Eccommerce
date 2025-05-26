3a158539a3c7ef79cc4f308de519d6a174ee0d14
blue-romeo <romeoangasa@gmail.com>
2025-05-26 16:53:13 +0300

    Price component update

--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -3,8 +3,24 @@ import './Price.css'
 
 const Price = () => {
   return (
-    <div>
-      Price
+    <div className='ml'>
+      <h2 className="sidebar-title">Price</h2>
+      <label  className="sidebar-label-container">
+          <input type="radio" name='test2' />
+          <span className="checkmark"></span>All
+        </label>
+        <label  className="sidebar-label-container">
+          <input type="radio" name='test2' />
+          <span className="checkmark"></span>$0 - $50
+        </label>
+        <label  className="sidebar-label-container">
+          <input type="radio" name='test2' />
+          <span className="checkmark"></span>$50 - $100
+        </label>
+        <label  className="sidebar-label-container">
+          <input type="radio" name='test2' />
+          <span className="checkmark"></span>$100 - $1502
+        </label>
     </div>
   )
 }
