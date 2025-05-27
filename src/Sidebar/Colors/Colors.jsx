4a432c28e2d96ea8a99a6b08fd0cb0adea9e5d46
blue-romeo <romeoangasa@gmail.com>
2025-05-27 12:45:05 +0300

    update Sidebar Colors component

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -4,7 +4,27 @@ import './Colors.css'
 const Colors = () => {
   return (
     <div>
-      Colors
+      <h2>Color</h2>
+      <label  className="color-title">
+          <input type="radio" name='test2' />
+          <span className="checkmark"></span>All
+        /</label>
+        <label  className="color-title">
+          <input type="radio" name='test2' />
+          <span className="checkmark"></span>Black
+        </label>
+        <label  className="color-title">
+          <input type="radio" name='test2' />
+          <span className="checkmark"></span>White
+        </label>
+        <label  className="color-title">
+          <input type="radio" name='test2' />
+          <span className="checkmark"></span>Red
+        </label>
+        <label  className="color-title">
+          <input type="radio" name='test2' />
+          <span className="checkmark"></span>Blue
+        </label>
     </div>
   )
 }
