50ea3353c8dfe1db89779ab176f8096307b8d1b9
blue-romeo <romeoangasa@gmail.com>
2025-05-29 15:15:25 +0300

    update on colors and price components

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -2,14 +2,53 @@ import React from 'react'
 import './Colors.css'
 import Input from '../../Components/Input'
 
-const Colors = () => {
+const Colors = ({handleChange}) => {
   return (
     <div>
-      <h2>Color</h2>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
+      <h2 className="sidebar-title">Color</h2>
+      <label  className="sidebar-label-container">
+          <input 
+          type="radio" 
+          name="test3" 
+          value="" 
+          onChange={handleChange} 
+          />
+          <span className="checkmark all"> </span>All
+          </label>
+          
+      <Input 
+          type="radio" 
+          name="test3" 
+          color='black'
+          onChange={handleChange} 
+          title='Black' 
+        /><Input 
+          type="radio" 
+          name='test3' 
+          color= 'green'
+          onChange={handleChange} 
+          title= 'Green'
+        /><Input 
+          type="radio" 
+          name='test3' 
+          color= 'red' 
+          onChange={handleChange} 
+          title= 'Red' 
+        /><Input 
+          type="radio" 
+          name="test3" 
+          color= 'blue' 
+          onChange={handleChange} 
+          title= 'Blue'
+        /><label  className="sidebar-label-container">
+          <input 
+          type="radio" 
+          name="test3" 
+          value="white" 
+          onChange={handleChange} 
+          />
+          <span className="checkmark" style={{background: "white", border: "2px solid black"}}> </span>White
+          </label>
     </div>
   )
 }
--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -2,14 +2,45 @@ import React from 'react'
 import './Price.css'
 import Input from '../../Components/Input'
 
-const Price = () => {
+const Price = ({handleChange}) => {
   return (
     <div className='ml'>
       <h2 className="sidebar-title">Price</h2>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
+      <label  className="sidebar-label-container">
+          <input 
+          type="radio" 
+          name="test2" 
+          value="" 
+          onChange={handleChange} 
+          />
+          <span className="checkmark"> </span>All
+          </label>
+          
+      <Input 
+          type="radio" 
+          name="test2" 
+          value={50} 
+          onChange={handleChange} 
+          title="$0-$50" 
+        /><Input 
+          type="radio" 
+          name="test2" 
+          value= {100} 
+          onChange={handleChange} 
+          title="$50-$100" 
+        /><Input 
+          type="radio" 
+          name="test2" 
+          value={150} 
+          onChange={handleChange} 
+          title="$150-$200" 
+        /><Input 
+          type="radio" 
+          name="test2" 
+          value= {200} 
+          onChange={handleChange} 
+          title="Over $200" 
+        />
     </div>
   )
 }
